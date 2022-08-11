cff11a028c9cfd234112e06de477aaf6a93d9e97
Andrew <andrewcrawford0000@gmail.com>
2022-08-10 19:07:34 -0500

    a

--- getstats.go
+++ getstats.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -17,8 +18,15 @@ func GetStats(username string) (string, error) {
 	}
 	defer resp.Body.Close()
 
+	//Create a variable of the same type as our model
+	var cResp datapoints
+
 	// Decode the data
-	//   if err := json.NewDecoder(resp.Body).Decode(&cResp); err != nil {
-	//	  log.Fatal("ooopsss! an error occurred, please try again")
-	//   }
+	if err := json.NewDecoder(resp.Body).Decode(&cResp); err != nil {
+		log.Fatal("ooopsss! an error occurred, please try again")
+	}
+
+	//Invoke the text output function & return it with nil as the error value
+	return cResp.TextOutput(), nil
+
 }
--- ui.go
+++ ui.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"io/ioutil"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
