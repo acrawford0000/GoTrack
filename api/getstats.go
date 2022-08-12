b2d11ed72909b22bad3e80bb38d879eb22a2839d
Andrew <andrewcrawford0000@gmail.com>
2022-08-12 15:49:25 -0500

    a

--- api/getstats.go
+++ api/getstats.go
@@ -8,29 +8,36 @@ import (
 )
 
 // Get stats is exported ...
-func GetStats(userid string) {
-	// Build The URL string
-	URL := "https://osutrack-api.ameo.dev/stats_history?user=" + userid + "&mode=0"
+func GetStats([]string) {
+	// Declare the slice where player ids will be stored
+	var Ids []string
 
-	// We make HTTP request using the Get function
-	resp, err := http.Get(URL)
-	if err != nil {
-		log.Fatal("an error occurred, please try again")
-	}
-	defer resp.Body.Close()
+	// For loop to run the stats for however many ids are in the slice
+	for _, userid := range Ids {
 
-	//Create a variable of the same type as the model, and create decoder
-	var CResp datapoints
+		// Build The URL string
+		URL := "https://osutrack-api.ameo.dev/stats_history?user=" + userid + "&mode=0"
 
-	// Decode the data
-	for {
-		if err := json.NewDecoder(resp.Body).Decode(&CResp); err != nil {
-			if err == io.EOF {
-				break
-			}
+		// We make HTTP request using the Get function
+		resp, err := http.Get(URL)
+		if err != nil {
 			log.Fatal("an error occurred, please try again")
+		}
+		defer resp.Body.Close()
+
+		// Create a variable of the same type as the model, and create decoder
+		var CResp datapoints
+
+		// Decode the data
+		for {
+			if err := json.NewDecoder(resp.Body).Decode(&CResp); err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatal("an error occurred, please try again")
+			}
+
 		}
 
 	}
-
 }
