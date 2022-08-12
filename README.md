a9816c41a4cbfcb1ffb7b47292477aeb545b4f23
Andrew <andrewcrawford0000@gmail.com>
2022-08-12 08:59:22 -0500

    Regenerated project with WAILS

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# GoTrack
+# README
+
+## About
+
+This is Wails 2.0 + ReactJS Template
+Tested on Windows and MacOS
+
+To create a project using this template run:
+`wails init -n [Your Appname] -t https://github.com/AlienRecall/wails-react-template`
+
+## Building 
+
+To build this project use `wails build`.
+
+## Live Development
+
+To run in live development mode, run `wails dev` in the project directory. The frontend dev server will run on http://localhost:34115. Wails will watch and re-build for every backend (golang) changes.
+
+## Known Issues
+
+Sadly I still not found a way to correclty run frontend in development mode so the only solution is to rebuild at every frontend change (if you got a way open a pull request thanks)
--- main.go
+++ main.go
@@ -1,17 +1,68 @@
 package main
 
 import (
-	"project/fyne"
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
+//go:embed frontend/build
+var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
+
 func main() {
+	// Create an instance of the app structure
+	app := NewApp()
 
-	fyne.MainFyne()
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "gotrack",
+		Width:  1024,
+		Height: 768,
+		// MinWidth:          720,
+		// MinHeight:         570,
+		// MaxWidth:          1280,
+		// MaxHeight:         740,
+		DisableResize:     false,
+		Fullscreen:        false,
+		Frameless:         false,
+		StartHidden:       false,
+		HideWindowOnClose: false,
+		RGBA:              &options.RGBA{255, 255, 255, 255},
+		Assets:            assets,
+		LogLevel:          logger.DEBUG,
+		OnStartup:         app.startup,
+		OnDomReady:        app.domReady,
+		OnShutdown:        app.shutdown,
+		Bind: []interface{}{
+			app,
+		},
+		// Windows platform specific options
+		Windows: &windows.Options{
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+			DisableWindowIcon:    false,
+		},
+		Mac: &mac.Options{
+			TitleBar:             mac.TitleBarHiddenInset(),
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  true,
+			About: &mac.AboutInfo{
+				Title:   "ReactJS Template",
+				Message: "Part of the Wails projects",
+				Icon:    icon,
+			},
+		},
+	})
 
-	// Call fn GetStats
-	//stats, err := GetStats(userid)
-	//if err != nil {
-	//	log.Fatal("ooopsss an error occurred, please try again")
-	//}
-
+	if err != nil {
+		log.Fatal(err)
+	}
 }
