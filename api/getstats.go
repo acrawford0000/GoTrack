9331969502dd433646da40d71b4893c18ac27bee
Andrew <andrewcrawford0000@gmail.com>
2022-08-14 12:20:35 -0500

    Rewrite to use Fyne GUI

--- api/getstats.go
+++ api/getstats.go
@@ -3,14 +3,16 @@ package api
 import (
 	"encoding/json"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+// Declare the slice where player ids will be stored
+var Ids []string
+
 // Get stats is exported ...
 func GetStats([]string) {
-	// Declare the slice where player ids will be stored
-	var Ids []string
 
 	// For loop to run the stats for however many ids are in the slice
 	for _, userid := range Ids {
@@ -18,19 +20,19 @@ func GetStats([]string) {
 		// Build The URL string
 		URL := "https://osutrack-api.ameo.dev/stats_history?user=" + userid + "&mode=0"
 
-		// We make HTTP request using the Get function
+		// Make HTTP request using the Get function
 		resp, err := http.Get(URL)
 		if err != nil {
 			log.Fatal("an error occurred, please try again")
 		}
 		defer resp.Body.Close()
 
-		// Create a variable of the same type as the model, and create decoder
-		var CResp datapoints
+		// Make var for json data to go into
+		var History []datapoints
 
 		// Decode the data
 		for {
-			if err := json.NewDecoder(resp.Body).Decode(&CResp); err != nil {
+			if err := json.NewDecoder(resp.Body).Decode(&History); err != nil {
 				if err == io.EOF {
 					break
 				}
@@ -38,6 +40,21 @@ func GetStats([]string) {
 			}
 
 		}
-
+		jsonData, err := json.MarshalIndent(History, "", " ")
+		if err != nil {
+			panic(err)
+		}
+		err = ioutil.WriteFile("stats.json", jsonData, 0644)
+		if err != nil {
+			panic(err)
+		}
 	}
+
+	// This is to parse the date to Y/M/D
+	//date, err := time.Parse("2015-04-21T01:23:21.000Z", timestamp)
+	//if err != nil {
+	//	panic(err)
+	//	}
+	// 	date.Format("2006-01-02")
+
 }
--- api/modelstats.go
+++ api/modelstats.go
@@ -1,21 +1,21 @@
 package api
 
 // Stats data is exported, it models the data we receive.
-type datapoints []struct {
-	count300      int
-	count100      int
-	count50       int
-	playcount     int
-	ranked_score  int
-	total_score   int
-	pp_rank       int
-	level         int
-	pp_raw        float64
-	accuracy      float64
-	count_rank_ss int
-	count_rank_s  int
-	count_rank_a  int
-	timestamp     string
+type datapoints struct {
+	Count300    int     `json:"count300"`
+	Count100    int     `json:"count100"`
+	Count50     int     `json:"count50"`
+	Playcount   int     `json:"playcount"`
+	RankedScore string  `json:"ranked_score"`
+	TotalScore  string  `json:"total_score"`
+	PpRank      int     `json:"pp_rank"`
+	Level       float64 `json:"level"`
+	PpRaw       float64 `json:"pp_raw"`
+	Accuracy    float64 `json:"accuracy"`
+	CountRankSS int     `json:"count_rank_ss"`
+	CountRankS  int     `json:"count_rank_s"`
+	CountRankA  int     `json:"count_rank_a"`
+	Timestamp   string  `json:"timestamp"`
 }
 
 // This is to parse the date to Y/M/D
