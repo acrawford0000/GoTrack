19eb7d666d24a4cf29d1b0a388a5cdc0e2a1c086
Andrew <andrewcrawford0000@gmail.com>
2022-08-14 12:57:37 -0500

    user list in gui fixed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GoTrack
+
+A program to create graphs for any osu players and their stats based on the osutrack API.
+
+This project uses Fyne as a library forthe GUI. 
--- fyne/fyne.go
+++ fyne/fyne.go
@@ -16,31 +16,8 @@ func MainFyne() {
 	a := app.New()
 	w := a.NewWindow("GoTrack")
 
+	// Create welcome label at the top of the screen
 	welcome := widget.NewLabelWithStyle("Welcome to GoTrack! Make charts for everything osu!", fyne.TextAlignCenter, fyne.TextStyle{})
-
-	// Take input from user
-	input := widget.NewEntry()
-	input.SetPlaceHolder("Enter a userid")
-
-	// Create a label to display the result
-	result := widget.NewLabel("")
-	result.textAlignCenter()
-
-	// Create a save button
-	button := widget.NewButton("Save", func() {
-		// when the button is clicked set the result label
-		result.SetText("Saved: " + input.Text)
-		api.Ids = append(api.Ids, input.Text)
-	})
-
-	// Create a label to display file has been created
-	created := widget.NewLabel("")
-
-	// Create a new button that will send all the usernames through GetStats
-	createFile := widget.NewButton("Create File", func() {
-		created.SetText("File has been created for users")
-		api.GetStats(api.Ids)
-	})
 
 	// Create a list that displays stored userids
 	list := widget.NewList(
@@ -54,15 +31,37 @@ func MainFyne() {
 			o.(*widget.Label).SetText(api.Ids[i])
 		})
 
+	// Take input from user
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Enter a userid")
+
+	// Create a label to display the result
+	result := widget.NewLabelWithStyle("", fyne.TextAlignCenter, fyne.TextStyle{})
+
+	// Create a save button
+	button := widget.NewButton("Save", func() {
+		// when the button is clicked set the result label
+		result.SetText("User added to list!")
+		api.Ids = append(api.Ids, input.Text)
+		list.Refresh()
+	})
+
+	// Create a label to display file has been created
+	created := widget.NewLabel("")
+
+	// Create a new button that will send all the usernames through GetStats
+	createFile := widget.NewButton("Create File", func() {
+		created.SetText("File has been created for users")
+		api.GetStats(api.Ids)
+	})
+
 	//Create containers to use in layout
 	welcomelabel := container.NewCenter(welcome)
 	content := container.NewVBox(input, button, result, createFile, created)
-	idlist := container.NewVScroll(list)
 
 	// Create container for everything
-	//pad := container.NewPadded(content)
-	border := container.New(layout.NewBorderLayout(welcomelabel, nil, idlist, nil),
-		welcomelabel, idlist, content)
+	border := container.New(layout.NewBorderLayout(welcomelabel, nil, list, nil),
+		welcomelabel, list, content)
 
 	w.SetContent(border)
 	w.ShowAndRun()
